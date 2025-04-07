0e7268b9feaf861e391cd5489ad8d816b32eb0c1
Anna Beatriz <05annabeaoli@gmail.com>
2025-04-07 11:45:35 -0300

    Atividades 04 e 05

---
+++ Atividade04.py
@@ -0,0 +1,6 @@
+soma=0
+for a in range(1,11,1):
+    num = float(input("Digite o número: "))
+    soma = soma + num
+print(soma)
+
---
+++ Atividade05.py
@@ -0,0 +1,4 @@
+num  = int(input("Digite um número: "))
+for a in range(1,11,1):
+    res = a * num
+    print(f"{a} x {num} = {res}")
